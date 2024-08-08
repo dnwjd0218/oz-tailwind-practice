1cab4d7c37d5151fb04d22094c15b2ea60b32cbc
dnwjd0218 <dnwjd1325@naver.com>
2024-08-09 01:36:11 +0900

    tailwindcss 12일차과제

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+// section {
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: center;
+//   gap: 20px;
+//   padding: 20px 40px;
+// }
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-5 px-4 py-2">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="flex justify-between px-2 py-1 bg-black align-center">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="flex justify-center gap-5 align-center">
+        <li className="text-xs font-normal list-none">로그인</li>
+        <li className="text-xs font-normal list-none">회원가입</li>
+        <li className="text-xs font-normal list-none">내클래스</li>
       </ul>
     </header>
   );
 }
+// .header-container {
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding: 10px 20px;
+//   background-color: black;
+// }
+
+// .header-container ul {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   gap: 20px;
+// }
+
+// ul li {
+//   list-style: none;
+//   font-weight: 400;
+//   font-size: 13px;
+// }
